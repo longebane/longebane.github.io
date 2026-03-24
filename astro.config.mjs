d950d3db6d824dc6c0b4a620fd7f780d885ddcc3
Long Dao <longdao@studiodao.com>
2026-03-24 15:33:15 -0700

    update seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://longebane.github.io",
+  site: "https://studiodao.com",
   base: "/",
 });
