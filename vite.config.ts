65ce5b361ff8fa20f10eeb9b28e01636b7f1f7ec
Long Dao <longdao@studiodao.com>
2026-03-26 17:28:43 -0700

    chore: configure VS Code and Vite for consistent code formatting with Oxc and Astro.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
+  fmt: {
+    // You can put oxfmt (Prettier compatible) options here
+    // e.g. singleQuote: true,
+  },
   staged: {
     "*": "vp check --fix",
   },
